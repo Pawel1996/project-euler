93914ace280c65c7a101bf1b09198c3b94cd1c21
Pawel <pawel1996k@gmail.com>
2022-01-20 15:20:27 +0100

    Solved problem5 - answer correct.

--- src/main/java/solutions/Problem5.java
+++ src/main/java/solutions/Problem5.java
@@ -7,14 +7,17 @@ public class Problem5 {
         long answer = -1;
         boolean found = false;
 
-        for (long i = 20; i < Long.MAX_VALUE; i++) {
+        for (long i = upperBound; i < Long.MAX_VALUE; i++) {
+            found = true;
             for (int j = 1; j <= upperBound; j++) {
                 if (i % j != 0) {
+                    found = false;
                     break;
                 }
-                if (i % upperBound == 0) {
-                    return i;
-                }
+            }
+            if (found) {
+                answer = i;
+                break;
             }
         }
 
