81a2a4f95740c2b9d457029f930540936c5cbf9f
Pawel <pawel1996k@gmail.com>
2022-01-21 18:30:42 +0100

    Problem 9 - narrowed down number of iterations. Added comments.

--- src/main/java/solutions/Problem9.java
+++ src/main/java/solutions/Problem9.java
@@ -4,23 +4,27 @@ public class Problem9 {
     private static final int sum = 1000;
     private static final int exponent = 2;
 
-    public static int solve() {
-        int product = -1;
+    // i cannot be bigger than 333, because j and k would have to be equal to at least 334 and 335 respectively.
+    // Then the sum would be 1002 at least, which is wrong.
+    // Similarly, j couldn't be equal to more than 500, because then k would have to be at least 501, and the sum would
+    // also be at least 1002.
+    // As for now I can't think of a better way to narrow it down, but this one works really fast for the given
+    // numbers, even though there are 3 loops nested in each other.
 
-        for (int i = 1; i < sum; i++) {
-            for (int j = i + 1; j < sum; j++) {
-                for (int k = j + 1; k < sum; k++) {
+    public static int solve() {
+        for (int i = 1; i <= sum / 3; i++) {
+            for (int j = i + 1; j <= sum / 2; j++) {
+                for (int k = j + 1; i + j + k <= sum; k++) {
                     if (Math.pow(i, exponent) + Math.pow(j, exponent) == Math.pow(k, exponent)) {
-//                        System.out.println(i + ", " + j + ", " + k);
+                        System.out.println(i + ", " + j + ", " + k);
                         if (i + j + k == sum) {
-                            product = i * j * k;
-                            System.out.println("Those numbers are: " + i + ", " + j + ", " + k);
+                            return i * j * k;
                         }
                     }
                 }
             }
         }
 
-        return product;
+        return -1;
     }
 }
