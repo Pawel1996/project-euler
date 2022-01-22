52dadafabf31cfe28550e96325f7c7f0d63a8de7
Pawel <pawel1996k@gmail.com>
2022-01-22 14:41:02 +0100

    Solved problem10. I think the solution is not optimal, but it works. Probably there should be a simpler solution to the chcecking of number 2.

--- src/main/java/solutions/Problem10.java
+++ src/main/java/solutions/Problem10.java
@@ -1,22 +1,31 @@
 package solutions;
 
 public class Problem10 {
-
+    private static final int upperBound = 2000000;
 
     public static long solve() {
+        long sum = 0;
 
-        for (long i = 0; i < 20; i++) {
-            System.out.println(i + ": " + isPrime(i));
+        for (long i = 0; i < upperBound; i++) {
+            if (isPrime(i)) {
+                sum += i;
+//                System.out.println(i);
+            }
         }
 
-        return -1;
+        return sum;
     }
     
     private static boolean isPrime(long number) {
+//        System.out.println(number + ": ");
         if (number <= 1) {
             return false;
         }
-        for (long i = 2; i < number; i++) {
+        if (number == 2) {
+            return true;
+        }
+        for (long i = 2; i <= Math.sqrt(number); i++) {
+//            System.out.println(i);
             if (number % i == 0) {
                 return false;
             }
